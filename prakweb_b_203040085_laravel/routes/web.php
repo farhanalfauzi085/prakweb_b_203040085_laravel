7a9950f60fd0f076de2705242bd449907c593514
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-10-21 20:25:59 +0700

    membuat struktur folder, router & view

--- prakweb_b_203040085_laravel/routes/web.php
+++ prakweb_b_203040085_laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Farhan Alfauzi",
+        "email" => "203040085@mail.unpas.ac.id",
+        "image" => "profil.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
