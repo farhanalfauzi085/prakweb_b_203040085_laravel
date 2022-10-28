ac562b93160b2ec73687ab37ea4917cd3eab60fa
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-10-29 01:11:08 +0700

    menerapkan model, collection & controller

--- prakweb_b_203040085_laravel/routes/web.php
+++ prakweb_b_203040085_laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,54 +31,8 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Farhan Alfauzi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora ipsum vero dolor. At numquam reiciendis itaque dolores voluptatem praesentium doloribus illo vero laborum laboriosam porro illum blanditiis amet eaque cumque nisi, quibusdam nemo eligendi nihil delectus cum. Aliquid eum aspernatur nisi sapiente ipsam, nemo molestiae unde facilis sequi nihil ullam magnam vero soluta ea? Consequatur rerum nemo ullam. Nisi rerum sapiente reiciendis iste, iusto harum, animi sequi aliquid nihil alias optio reprehenderit cum amet adipisci! Doloribus adipisci dolorem harum?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Hamzah Hadi Permana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora ipsum vero dolor. At numquam reiciendis itaque dolores voluptatem praesentium doloribus illo vero laborum laboriosam porro illum blanditiis amet eaque cumque nisi, quibusdam nemo eligendi nihil delectus cum. Aliquid eum aspernatur nisi sapiente ipsam, nemo molestiae unde facilis sequi nihil ullam magnam vero soluta ea? Consequatur rerum nemo ullam. Nisi rerum sapiente reiciendis iste, iusto harum, animi sequi aliquid nihil alias optio reprehenderit cum amet adipisci! Doloribus adipisci dolorem harum? quibusdam nemo eligendi nihil delectus cum. Aliquid eum aspernatur nisi sapiente ipsam, nemo molestiae unde facilis sequi nihil ullam magnam vero soluta ea? Consequatur rerum nemo ullam. Nisi rerum sapiente reiciendis iste, iusto harum."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 
 // halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Farhan Alfauzi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora ipsum vero dolor. At numquam reiciendis itaque dolores voluptatem praesentium doloribus illo vero laborum laboriosam porro illum blanditiis amet eaque cumque nisi, quibusdam nemo eligendi nihil delectus cum. Aliquid eum aspernatur nisi sapiente ipsam, nemo molestiae unde facilis sequi nihil ullam magnam vero soluta ea? Consequatur rerum nemo ullam. Nisi rerum sapiente reiciendis iste, iusto harum, animi sequi aliquid nihil alias optio reprehenderit cum amet adipisci! Doloribus adipisci dolorem harum?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Hamzah Hadi Permana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora ipsum vero dolor. At numquam reiciendis itaque dolores voluptatem praesentium doloribus illo vero laborum laboriosam porro illum blanditiis amet eaque cumque nisi, quibusdam nemo eligendi nihil delectus cum. Aliquid eum aspernatur nisi sapiente ipsam, nemo molestiae unde facilis sequi nihil ullam magnam vero soluta ea? Consequatur rerum nemo ullam. Nisi rerum sapiente reiciendis iste, iusto harum, animi sequi aliquid nihil alias optio reprehenderit cum amet adipisci! Doloribus adipisci dolorem harum? quibusdam nemo eligendi nihil delectus cum. Aliquid eum aspernatur nisi sapiente ipsam, nemo molestiae unde facilis sequi nihil ullam magnam vero soluta ea? Consequatur rerum nemo ullam. Nisi rerum sapiente reiciendis iste, iusto harum."
-        ]
-    ];
-
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
