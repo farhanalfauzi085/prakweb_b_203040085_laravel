327756242cca4e002065c5413ba13bf11597c1a6
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-10-29 21:46:41 +0700

    membuat database seeder

--- prakweb_b_203040085_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_b_203040085_laravel/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +24,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Farhan Alfauzi',
+            'email' => '203040085@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'M. Rafliansyah',
+            'email' => '203040105@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim. Laudantium odio recusandae, quam eveniet alias maiores minima, ea, enim exercitationem magni vero. Blanditiis sit, maiores sunt iusto ea maxime iure magnam voluptatibus at! Eum ducimus alias sed doloremque recusandae ipsa atque deleniti beatae numquam quisquam explicabo cupiditate sit fuga, mollitia quasi, dignissimos aliquam voluptas voluptatum architecto at. Qui quos amet velit architecto pariatur est. Explicabo vero eveniet neque ad! At iure perferendis debitis quas impedit, consequatur cumque culpa soluta sed asperiores hic, repellat voluptatibus velit reprehenderit eaque recusandae eum animi corporis possimus voluptas!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim. Laudantium odio recusandae, quam eveniet alias maiores minima, ea, enim exercitationem magni vero. Blanditiis sit, maiores sunt iusto ea maxime iure magnam voluptatibus at! Eum ducimus alias sed doloremque recusandae ipsa atque deleniti beatae numquam quisquam explicabo cupiditate sit fuga, mollitia quasi, dignissimos aliquam voluptas voluptatum architecto at. Qui quos amet velit architecto pariatur est. Explicabo vero eveniet neque ad! At iure perferendis debitis quas impedit, consequatur cumque culpa soluta sed asperiores hic, repellat voluptatibus velit reprehenderit eaque recusandae eum animi corporis possimus voluptas!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim. Laudantium odio recusandae, quam eveniet alias maiores minima, ea, enim exercitationem magni vero. Blanditiis sit, maiores sunt iusto ea maxime iure magnam voluptatibus at! Eum ducimus alias sed doloremque recusandae ipsa atque deleniti beatae numquam quisquam explicabo cupiditate sit fuga, mollitia quasi, dignissimos aliquam voluptas voluptatum architecto at. Qui quos amet velit architecto pariatur est. Explicabo vero eveniet neque ad! At iure perferendis debitis quas impedit, consequatur cumque culpa soluta sed asperiores hic, repellat voluptatibus velit reprehenderit eaque recusandae eum animi corporis possimus voluptas!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim.',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque repudiandae odit tempore itaque harum ut enim. Laudantium odio recusandae, quam eveniet alias maiores minima, ea, enim exercitationem magni vero. Blanditiis sit, maiores sunt iusto ea maxime iure magnam voluptatibus at! Eum ducimus alias sed doloremque recusandae ipsa atque deleniti beatae numquam quisquam explicabo cupiditate sit fuga, mollitia quasi, dignissimos aliquam voluptas voluptatum architecto at. Qui quos amet velit architecto pariatur est. Explicabo vero eveniet neque ad! At iure perferendis debitis quas impedit, consequatur cumque culpa soluta sed asperiores hic, repellat voluptatibus velit reprehenderit eaque recusandae eum animi corporis possimus voluptas!',
+            'category_id' => 1,
+            'user_id' => 2
+        ]);
     }
 }
--- prakweb_b_203040085_laravel/resources/views/post.blade.php
+++ prakweb_b_203040085_laravel/resources/views/post.blade.php
@@ -3,10 +3,10 @@
 @section('container')
 <article>
   <h2>{{ $post->title }}</h2>
-  <p>By: Farhan Alfauzi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
   {!! $post->body !!}
 </article>
 
-<a href="/blog">Back to posts</a>
+<a href="/blog" class="text-decoration-none">Back to posts</a>
 
 @endsection
