be7194b1661556052bfa5acd47b794febc990923
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-10-29 18:32:58 +0700

    membuat model untuk category post & menggunakan fitur eloquent relationship

--- prakweb_b_203040085_laravel/resources/views/post.blade.php
+++ prakweb_b_203040085_laravel/resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 <article>
   <h2>{{ $post->title }}</h2>
-  <!-- <h5>{{ $post["author"] }}</h5> -->
+  <p>By: Farhan Alfauzi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
   {!! $post->body !!}
 </article>
 
