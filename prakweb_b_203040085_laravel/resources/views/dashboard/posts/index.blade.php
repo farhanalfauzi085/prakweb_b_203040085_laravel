0e88e08dc27fb6dcb0c164036086f5ecb8b6ff31
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-11-29 22:23:58 +0700

    menambahkan fitur validasi & insert data post

--- prakweb_b_203040085_laravel/resources/views/dashboard/posts/index.blade.php
+++ prakweb_b_203040085_laravel/resources/views/dashboard/posts/index.blade.php
@@ -5,6 +5,12 @@
   <h1 class="h2">My Posts</h1>
 </div>
 
+@if(session()->has('succes'))
+<div class="alert alert-success" role="alert">
+  {{ session('succes') }}
+</div>
+@endif
+
 <div class="table-responsive col-lg-8">
   <a href="/dashboard/posts/create" class="btn btn-dark mb-3">Create new post</a>
   <table class="table table-striped table-sm">
