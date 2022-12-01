2c44e508f46983c5c9acf87cb5d1e123e601af8a
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-12-01 15:58:57 +0700

    menambahkan fitur preview, update & delete gambar

--- prakweb_b_203040085_laravel/app/Http/Controllers/DashboardPostController.php
+++ prakweb_b_203040085_laravel/app/Http/Controllers/DashboardPostController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Category;
+// use Clockwork\Storage\Storage;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -101,6 +103,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
 
@@ -110,6 +113,13 @@ class DashboardPostController extends Controller
         }
 
         $validateData = $request->validate($rules);
+
+        if($request->file('image')) {
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validateData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validateData['user_id'] = auth()->user()->id;
         $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -128,6 +138,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
