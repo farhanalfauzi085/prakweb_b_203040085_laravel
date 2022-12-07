3b006f3f561fe0648807f990f0d0212fbc101e85
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-12-07 11:44:22 +0700

    menambahkan fitur authorization

--- prakweb_b_203040085_laravel/app/Providers/AppServiceProvider.php
+++ prakweb_b_203040085_laravel/app/Providers/AppServiceProvider.php
@@ -3,8 +3,12 @@
 namespace App\Providers;
 
 // use Illuminate\Contracts\Pagination\Paginator;
+
+// use Illuminate\Auth\Access\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +30,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
+        });
     }
 }
