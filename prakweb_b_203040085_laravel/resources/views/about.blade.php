2dbebd2d3bbdc8dd76b85914358816f82dbfd7f3
farhanalfauzi085 <203040085@mail.unpas.ac.id>
2022-12-13 21:18:19 +0700

    merubah halaman home & about

--- prakweb_b_203040085_laravel/resources/views/about.blade.php
+++ prakweb_b_203040085_laravel/resources/views/about.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Halaman About</h1>
-<h3>{{ $name }}</h3>
-<p>{{ $email }}</p>
-<img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
+<header>
+  <div class="container text-center">
+    <h1>About Me</h3>
+    <hr>
+    <br>
+    <h5>{{ $name }}</h5>
+    <p>{{ $email }}</p>
+    <a href="https://www.instagram.com/farhanalfauziii/" class="btn btn-dark" target="_blank"><i class="fa fa-instagram"></i> My Instagram</a>
+    <br><br>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
+  </div>
+</header>
 @endsection
--- prakweb_b_203040085_laravel/resources/views/home.blade.php
+++ prakweb_b_203040085_laravel/resources/views/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Halaman Home</h1>
+<!-- <h1>Halaman Home</h1> -->
+<header>
+  <div class="container text-center">
+    <h1 class="nprinsley-text-glitchan">Oops!</h1>
+    <h3>This website is made simply.</h3>
+    <h6>use this website wisely.</h6>
+  </div>
+</header>
 @endsection
